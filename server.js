7160c144cbfd2b9d161d1a534d138b5510608075
Rupesh Yadav <rupeshyadav8178@gmail.com>
2022-11-30 15:52:06 +0530

    merge

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const session = require("express-session");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-<<<<<<< HEAD
-const path = require('path');
+const path = require("path");
 const mongoose = require("mongoose");
-const imageRoutes =require('./routes/lost_imgRoutes');
+const imageRoutes = require("./routes/lost_imgRoutes");
 const signup = require("./routes/signup");
 const login = require("./routes/login");
 const event = require("./routes/event");
@@ -21,25 +20,28 @@ const app = express();
 app.use(bodyParser.json());
 // app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.DATABASE_URL,
-    { usenewUrlParser: true,useUnifiedTopology:true })
-    .then(() => {
-     console.log('You are connected to dsw')
-     // app.listen(3000);
- })
- .catch((error) => {
-     console.log('Connection to dsw failed', error)
- });
- app.use(express.static(__dirname + "public")); //Serves resources from public folder
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    usenewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("You are connected to dsw");
+    // app.listen(3000);
+  })
+  .catch((error) => {
+    console.log("Connection to dsw failed", error);
+  });
+app.use(express.static(__dirname + "public")); //Serves resources from public folder
 app.use("/Images", express.static(__dirname + "/public/FoundImage"));
 app.use("/images", express.static(__dirname + "/public/EventCovers"));
 app.use(
-    cookieSession({
-      name: "dsw-session",
-      secret: secret, // should use as secret environment variable
-      httpOnly: true,
-    })
-  );
+  cookieSession({
+    name: "dsw-session",
+    secret: secret, // should use as secret environment variable
+    httpOnly: true,
+  })
+);
 
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -52,8 +54,10 @@ app.use("/login", login);
 app.use("/event", event);
 app.use("/manage", manage);
 
-app.use('/lost', imageRoutes );
-app.get("/", (req, res) => { res.send("working") });
+app.use("/lost", imageRoutes);
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
 // const db = mongo.connection;
 // client.db("DSW").collection("images");
@@ -63,26 +67,6 @@ app.get("/", (req, res) => { res.send("working") });
 // db.once("open", () => {
 //   console.log("connection complete");
 // });
-=======
-const signup = require("./routes/signup");
-const login = require("./routes/login");
-const event = require("./routes/event");
-const manage = require("./routes/manage");
-const cookieSession = require("cookie-session");
-
-const mongo = require("mongoose");
-const { secret } = require("./config/auth.config");
-mongo.connect(process.env.DATABASE_URL, { usenewUrlParser: true });
-const db = mongo.connection;
-db.on("error", (error) => {
-  console.error(error);
-});
-db.once("open", () => {
-  console.log("Mongodb connection complete");
-});
-app.use(express.static(__dirname + "public")); //Serves resources from public folder
-app.use("/images", express.static(__dirname + "/public/EventCovers"));
->>>>>>> 5d83e7b3f5ec0c310de112014bbef31db10e3454
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -94,11 +78,6 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 5d83e7b3f5ec0c310de112014bbef31db10e3454
 const port = process.env.PORT || 3000;
 app.use("/signup", signup);
 app.use("/login", login);
